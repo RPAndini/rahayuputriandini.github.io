8b797aff544ab51c4ecddb1560cd576bfb078fe2
Rahayu Putri Andini <127615970+RPAndini@users.noreply.github.com>
2024-10-20 08:47:39 +0700

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,47 +1,16 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $to = 'rahayuputriandiniii@gmail.com';
+    $subject = $_POST['subject'];
+    $message = "Name: " . $_POST['name'] . "\n";
+    $message .= "Email: " . $_POST['email'] . "\n";
+    $message .= "Message: " . $_POST['message'] . "\n";
+    $headers = 'From: ' . $_POST['email'] . "\r\n";
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'rahayuputriandiniii@gmail.com';
-
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
-
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
-
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
-$contact->smtp = array(
-    'host' => 'rahayuputriandiniii@gmail.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
+    if (mail($to, $subject, $message, $headers)) {
+        echo 'Email sent successfully!';
+    } else {
+        echo 'Failed to send email.';
+    }
+}
 ?>
